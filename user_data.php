fe65170468bdbb1281b73bf64b7d9a3a9cf38214
Temmy <iyedetemitayo08@gmail.com>
2022-10-14 13:59:18 +0100

    first commit

--- user_data.php
+++ user_data.php
@@ -12,10 +12,32 @@ if (isset($_POST["submit"])) {
 
     $user = [$first_name, $last_name, $email, $dob, $gender, $country];
 
+    // The Output at the front end page.
+
+    echo "FIRST NAME: ";
+    print_r($first_name);
+    echo "<br>", "<br>";
+    echo "LAST NAME: ";
+    print_r($last_name);
+    echo "<br>", "<br>";
+    echo "EMAIL: ";
+    print_r($email);
+    echo "<br>", "<br>";
+    echo "DATE OF BIRTH: ";
+    print_r($dob);
+    echo "<br>", "<br>";
+    echo "GENDER: ";
+    print_r($gender);
+    echo "<br>", "<br>";
+    echo "COUNTRY: ";
+    print_r($country);
+    echo "<br>", "<br>";
+
+
     // saving the data in a csv file. "a" is used to write into the data without overwriting the other details in it.
 
     $filename = "user_data.csv";
     $handle = fopen($filename, "a");
-    fputcsv($handle, $user); // The implode helps to convert the array to string, since fwrite arguement will only take in a string.
+    fputcsv($handle, $user);
     fclose($handle);
 }
